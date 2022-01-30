c83320e1fcd81e6756ef3e4527a6f42b34e0b128
zz0108 <nn12572312@gmail.com>
2022-01-30 14:24:00 +0800

    Test Case Refactor

--- TennisGameTest/TennisTests.cs
+++ TennisGameTest/TennisTests.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using NUnit.Framework;
 using TennisGame;
 
@@ -7,113 +8,31 @@ public class TennisTests
 {
     private Tennis _tennis = new Tennis();
 
-    [Test]
-    public void LoveAll()
+    private static IEnumerable TestData()
     {
-        ScoreShouldBe("love all");
+        yield return new TestCaseData(0, 0, "love all");
+        yield return new TestCaseData(1, 0, "fifteen love");
+        yield return new TestCaseData(2, 0, "thirty love");
+        yield return new TestCaseData(3, 0, "forty love");
+        yield return new TestCaseData(0, 1, "love fifteen");
+        yield return new TestCaseData(0, 2, "love thirty");
+        yield return new TestCaseData(0, 3, "love forty");
+        yield return new TestCaseData(1, 1, "fifteen all");
+        yield return new TestCaseData(2, 2, "thirty all");
+        yield return new TestCaseData(3, 3, "deuce");
+        yield return new TestCaseData(4, 3, "Joey adv");
+        yield return new TestCaseData(3, 4, "Tom adv");
+        yield return new TestCaseData(5, 3, "Joey win");
+        yield return new TestCaseData(3, 5, "Tom win");
     }
-
-    [TestCase(1)]
-    public void FifteenLove(int firstPlayerScore)
+    
+    [Test]
+    [TestCaseSource(nameof(TestData))]
+    public void AllTest(int firstPlayerScore,int secondPlayerScore,string expected)
     {
         GivenFirstPlayerScore(firstPlayerScore);
-        ScoreShouldBe("fifteen love");
-    }
-
-    [Test]
-    public void ThirtyLove()
-    {
-        GivenFirstPlayerScore(2);
-        ScoreShouldBe("thirty love");
-    }
-
-    [Test]
-    public void FortyLove()
-    {
-        GivenFirstPlayerScore(3);
-        ScoreShouldBe("forty love");
-    }
-
-    [Test]
-    public void LoveFifteen()
-    {
-        GivenSecondPlayerScore(1);
-        ScoreShouldBe("love fifteen");
-    }
-
-    [Test]
-    public void LoveThirty()
-    {
-        GivenSecondPlayerScore(2);
-        ScoreShouldBe("love thirty");
-    }
-
-    [Test]
-    public void LoveForty()
-    {
-        GivenSecondPlayerScore(3);
-        ScoreShouldBe("love forty");
-    }
-
-    [Test]
-    public void FifteenAll()
-    {
-        GivenFirstPlayerScore(1);
-        GivenSecondPlayerScore(1);
-        ScoreShouldBe("fifteen all");
-    }
-
-    [Test]
-    public void ThirtyAll()
-    {
-        GivenFirstPlayerScore(2);
-        GivenSecondPlayerScore(2);
-        ScoreShouldBe("thirty all");
-    }
-
-    [Test]
-    public void Deuce()
-    {
-        GivenDeuce();
-        ScoreShouldBe("deuce");
-    }
-
-    [Test]
-    public void FirstPlayerAdv()
-    {
-        GivenDeuce();
-        GivenFirstPlayerScore(1);
-        ScoreShouldBe("Joey adv");
-    }
-
-    [Test]
-    public void SecondPlayerAdv()
-    {
-        GivenDeuce();
-        GivenSecondPlayerScore(1);
-        ScoreShouldBe("Tom adv");
-    }
-
-    [Test]
-    public void SecondPlayerWin()
-    {
-        GivenDeuce();
-        GivenSecondPlayerScore(2);
-        ScoreShouldBe("Tom win");
-    }
-
-    [Test]
-    public void FirstPlayerWin()
-    {
-        GivenDeuce();
-        GivenFirstPlayerScore(2);
-        ScoreShouldBe("Joey win");
-    }
-
-    private void GivenDeuce()
-    {
-        GivenFirstPlayerScore(3);
-        GivenSecondPlayerScore(3);
+        GivenSecondPlayerScore(secondPlayerScore);
+        ScoreShouldBe(expected);
     }
 
     private void GivenSecondPlayerScore(int times)
@@ -123,6 +42,7 @@ public class TennisTests
             _tennis.SecondPlayerScore();
         }
     }
+    
 
     private void GivenFirstPlayerScore(int times)
     {
